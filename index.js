d05a0d50927680142188825c65bec1b69ef8d7e1
Chase Anderson <chaseanderson.stu@gmail.com>
2023-11-01 13:51:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  const fareMultiplier = createFareMultiplier(2);
+
+  function fareDoubler(fare) {
+        return fare * 2;
+  }
+
+  function fareTripler(fare)    {
+    return fare * 3;
+  }
+
+  function selectDifferentDrivers(drivers, returnDriversFunction) {
+    if (returnDriversFunction === returnFirstTwoDrivers) {
+      return drivers.slice(0, 2);
+    } else if (returnDriversFunction === returnLastTwoDrivers) {
+      return drivers.slice(-2);
+    } else {
+      throw new Error('Invalid returnDriversFunction argument');
+    }
+  }
+
+  
